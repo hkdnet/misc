d2d6fb25dd7bae424a051f4238240b2ae8b3043f
hkdnet <hkdnet@users.noreply.github.com>
2016-06-07 00:18:04 +0900

    ファイ捨てるネットワークのやつ

---
+++ alice/feistel.rb
@@ -0,0 +1,34 @@
+input   = "1001011110110010".freeze
+sub_key = "10101101"
+
+def split_two(text)
+  len = text.length
+  half = len / 2
+  left = text[0, half]
+  right = text[half, half]
+  [left, right]
+end
+
+def exec_round(text, sub_key)
+  left, right = split_two(text)
+  ret = yield(right, sub_key)
+  il = left.to_i(2)
+  ir = ret.to_i(2)
+  new_left = (il ^ ir).to_s(2)
+  "#{new_left}#{right}"
+end
+
+done = exec_round(input.dup, sub_key) do |r, s|
+  (r.to_i(2) ^ s.to_i(2)).to_s(2)
+end
+
+done.freeze
+puts "sub_key: #{sub_key}"
+puts "input  : #{input}"
+puts "-" * 20
+puts "done   : #{done}"
+output = exec_round(done.dup, sub_key) do |r, s|
+  (r.to_i(2) ^ s.to_i(2)).to_s(2)
+end
+puts "output : #{output}"
+puts "input  : #{input}"
