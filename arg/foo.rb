de99059abcec1b3f45fb299f12b5e023eb4b5311
hkdnet <satoko.itse@gmail.com>
2018-06-10 21:50:56 +0900

    golf

--- arg/foo.rb
+++ arg/foo.rb
@@ -2,15 +2,9 @@ require 'pry'
 class Foo
   FOO__ARG1 = Object.new
   private_constant :FOO__ARG1
-  FOO__NAME = Object.new
-  private_constant :FOO__NAME
 
-
-  def foo(_arg1 = FOO__ARG1, name: FOO__NAME)
-    unless FOO__ARG1.equal?(_arg1)
-      name = _arg1
-    end
-    if FOO__NAME.equal?(name)
+  def foo(_arg1 = FOO__ARG1, name: _arg1)
+    if FOO__ARG1.equal?(name)
       raise ArgumentError
     end
 
